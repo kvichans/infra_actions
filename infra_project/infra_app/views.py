c9c38fa5d067b8691dfab329846ed8e893c1d419
kvichans <kvichans@mail.ru>
2021-06-21 13:17:58 +0000

    ok-ok

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('OK! У меня получилось!')
 
 
 def second_page(request):
-    return HttpResponse('А это вторая страница')
+    return HttpResponse('OK! А это вторая страница')
